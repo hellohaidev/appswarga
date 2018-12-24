d8de3c61082ae24d33ecb2cd84f7b2f94fc657c9
adirahman <kodingku@github.com>
2018-12-24 10:49:01 +0700

    update navigasi

--- nav.php
+++ nav.php
@@ -1,35 +1,38 @@
 <?php
 switch ($_GET['page']){
 
+	/**
+	 * data warga crud
+	 */
 	case 'add':
 		if ($_GET['page']){
-			include './inc/add.php';
+			include 'inc/warga/datawarga/add.php';
 			$content = $add;
 		}
         break;
     case 'delete':
 		if ($_GET['page']){
-			include './inc/delete.php';
+			include 'inc/warga/datawarga/delete.php';
 			$content = $delete;
 		}
 		break;
     case 'edit':
 		if ($_GET['page']){
-			include './inc/edit.php';
+			include 'inc/warga/datawarga/edit.php';
 			$content = $edit;
 		}
 		break;
 
-	case 'home':
+	case 'homewarga':
 		if ($_GET['page']){
-			include './inc/home.php';
+			include 'inc/warga/datawarga/home.php';
 			$content = $home;
 		}
 		break;
     
 	
 	default:
-		include './inc/home.php';
+		include 'inc/home.php';
 		$content = $home;
 		break;
 }
