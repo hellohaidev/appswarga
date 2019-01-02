d6c3c1df1f280283e37e39e967ac19572013d5bd
adirahman <kodingku@github.com>
2019-01-02 15:36:15 +0700

    update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if(isset($_POST['login'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Warga Apps</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/animate.css">
     <style>
